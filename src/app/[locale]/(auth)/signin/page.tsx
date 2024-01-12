4e1b5cde8c9fc3c2929345245f77134c02cdfc86
Vertox <gododani12@gmail.com>
2024-01-12 19:28:56 +0100

    Changed logo sizes and the input of phone number

--- src/app/[locale]/(auth)/signin/page.tsx
+++ src/app/[locale]/(auth)/signin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/legacy/image";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useTranslations } from "next-intl";
