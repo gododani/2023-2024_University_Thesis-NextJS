90f2b6af85a677bf27713b5220e764d709ee1bd2
Vertox <gododani12@gmail.com>
2024-04-19 20:17:07 +0200

    Fix CORS error for api #5

--- next.config.js
+++ next.config.js
@@ -2,20 +2,27 @@
 
 const withNextIntl = require("next-intl/plugin")();
 
+const getCorsHeaders = () => {
+  const headers = {};
+  
+  headers["Access-Control-Allow-Origin"] = "https://bekautomotor.hu/";
+  headers["Access-Control-Allow-Credentials"] = "true";
+  headers["Access-Control-Allow-Methods"] =
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT";
+  headers["Access-Control-Allow-Headers"] =
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization";
+
+  return Object.entries(headers).map(([key, value]) => ({ key, value }));
+};
+
 const nextConfig = {
-  async headers() {
+  headers: async () => {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
+        source: "/api/(.*)",
+        headers: getCorsHeaders(),
+      },
+    ];
   },
   images: {
     dangerouslyAllowSVG: true,
