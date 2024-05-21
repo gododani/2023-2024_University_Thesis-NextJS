d06efad5d27b2fff6c42fafee8cab3d2a4c20aa3
Vertox <gododani12@gmail.com>
2024-05-21 19:38:10 +0200

    Messenger test 4

--- src/components/messenger/Messenger.tsx
+++ src/components/messenger/Messenger.tsx
@@ -16,13 +16,13 @@ const Messenger = () => {
         <CustomChat pageId="2056191244610896" minimized={false} />
       </FacebookProvider> */}
       {/*message us*/}
-      {/* <FacebookProvider appId="769874795242233">
-        <MessageUs messengerAppId="roland.bek" pageId="2056191244610896" />
-      </FacebookProvider> */}
-      {/*send to messenger*/}
       <FacebookProvider appId="769874795242233">
-        <SendToMessenger messengerAppId="roland.bek" pageId="2056191244610896" />
+        <MessageUs messengerAppId="roland.bek" pageId="2056191244610896" />
       </FacebookProvider>
+      {/*send to messenger*/}
+      {/* <FacebookProvider appId="769874795242233">
+        <SendToMessenger messengerAppId="roland.bek" pageId="2056191244610896" />
+      </FacebookProvider> */}
       {/*messenger checkbox*/}
       {/* <FacebookProvider appId="769874795242233">
         <MessengerCheckbox
